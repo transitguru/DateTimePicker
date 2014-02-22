62818a72d717696d59f2868624757aee7d73a5a7
Michael Sypolt <msypolt@gmail.com>
2014-02-21 23:51:47 -0500

    GNU GPL3 License

--- js/datetimepicker.js
+++ js/datetimepicker.js
@@ -1,8 +1,24 @@
 /**
+ * A Lightweight Date Time Picker
+ * Copyright (C) 2014  Michael Sypolt <msypolt@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
  * @file
  * @author <msypolt@gmail.com>
- * 
- * Datepicker Functions 
+ *
+ * Datepicker JavaScript Functions 
  */
 "use strict";
 
